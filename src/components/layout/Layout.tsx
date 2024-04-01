82e0e2d24d273c2c4268a3017bfb051cb3c00e0c
yerinra <yerinaleph@gmail.com>
2024-04-01 16:06:30 +0900

    Initial settings

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <NavBar />
+    </div>
+  );
+}

9a4d3f440220b3a6133bcef748a2219e036cc82a
yerinra <yerinaleph@gmail.com>
2024-04-02 15:37:42 +0900

    [#3] design: 전체 레이아웃 구성

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import Header from "./Header";
-import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import Header from './Header';
+import NavBar from './NavBar';
+import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div>
+    <>
       <Header />
-      <Outlet />
+      <main className="w-full min-h-screen px-5">
+        <Outlet />
+      </main>
       <NavBar />
-    </div>
+    </>
   );
 }

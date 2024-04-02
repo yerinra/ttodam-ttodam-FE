427bc6bca1a1f1a91fb89073f6a45cbd984e7cad
yerinra <yerinaleph@gmail.com>
2024-04-02 14:17:52 +0900

    [#3] design: tailwind colors 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
       tablet: '769px',
       desktop: '1024px',
     },
+    extend: {
+      colors: {
+        primary: '#1E88E5',
+        secondary: '#64B5F6',
+        'light-gray': '#BCC4C6',
+        'dark-gray': '#828989',
+        black: '#000000',
+        orange: 'FF9B05',
+      },
+    },
   },
   plugins: [],
 };

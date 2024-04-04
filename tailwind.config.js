8d9a801e6c909b30e0913b9eb3edb9c9147c1ec6
K-DoHoon <144224546+K-DoHoon@users.noreply.github.com>
2024-04-05 07:09:21 +0900

    색상추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      tablet: '769px',
+      desktop: '1024px',
+    },
     extend: {
       colors: {
-        primary: '#4696D3',
-        second: '#f9e000',
+        primary: '#1E88E5',
+        secondary: '#64B5F6',
+        'light-gray': '#BCC4C6',
+        'dark-gray': '#828989',
+        black: '#000000',
+        orange: 'FF9B05',
+        yellow: '#f9e000',
       },
-      fontSize: {},
     },
   },
   plugins: [],

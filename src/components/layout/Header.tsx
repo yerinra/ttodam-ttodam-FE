39b7eef66bbed30070aa4ee0fc86618b206ffe1a
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-05 00:29:52 +0900

    [#5] feat: Header Input 컴포넌트 분리 -> Homepage로 이동

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
-import SearchInput from '../atoms/SearchInput';
 
 export default function Header() {
   // 헤더 280px 기준으로 min-width padding 양쪽 20px씩 빼고 240px로 설정
   return (
-    <header className="fixed flex w-full min-h-[50px] h-[160px] items-center flex-col px-5 pt-5 pb-8 gap-4 border-b min-w-[240px] bg-white z-[9999]">
+    <header className="fixed flex w-full min-h-[50px] h-[90px] items-center flex-col px-5 pt-5 pb-8 gap-4 border-b min-w-[240px] bg-white z-[9999]">
       <div className="flex w-full min-h-[50px] h-[96px] justify-between items-center">
         <h1 className="w-16 h-16">
           <Link to="/">
@@ -19,9 +18,6 @@ export default function Header() {
         >
           로그인
         </Link>
-      </div>
-      <div className="flex justify-center w-full">
-        <SearchInput />
       </div>
     </header>
   );

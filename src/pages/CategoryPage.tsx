b3e7e8da9631e5e0388816c900124505b2c13f12
yerinra <yerinaleph@gmail.com>
2024-04-04 18:21:41 +0900

    [#7] feat: 카테고리 페이지 구현

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
 import { CATEGORIES } from '../lib/data';
 import { useNavigate } from 'react-router-dom';
+import type { category } from '../lib/types';
 
 export default function CategoryPage() {
   const navigate = useNavigate();
+
+  const handleCategoryClick = (category: category) => {
+    navigate(`/post/${category}`);
+  };
+
   return (
     <section className="h-screen">
       <ul className="grid grid-cols-2 gap-y-3 tablet:gap-y-10 tablet:grid-cols-3 items-center justify-center tablet:p-5 tablet:pt-10">
         {CATEGORIES.map(category => (
           <li
-            onClick={() => {
-              navigate('/post');
-            }}
+            key={category.type}
+            onClick={() => handleCategoryClick(category.type)}
             className="flex flex-col items-center justify-center h-[100px]"
           >
             <button className="flex flex-col items-center gap-y-2 group">

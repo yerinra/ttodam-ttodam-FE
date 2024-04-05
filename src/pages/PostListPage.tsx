f79e5e3dfb5a0a0593ac745d046ad459ef738315
yerinra <yerinaleph@gmail.com>
2024-04-05 20:22:29 +0900

    [#11] feat: 게시판 페이지 상단 카테고리 UI 작업

--- src/pages/PostListPage.tsx
+++ src/pages/PostListPage.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import { CATEGORIES } from '../lib/data';
+import { useNavigate, useParams } from 'react-router-dom';
+import type { category } from '../lib/types';
 
 export default function PostListPage() {
-  return <div>PostList</div>;
+  const { categoryName } = useParams();
+  const navigate = useNavigate();
+
+  const handleCategoryChange = (newCategory: category) => {
+    navigate(`/post/${newCategory}`);
+  };
+
+  return (
+    <>
+      <section className="flex flex-wrap gap-2">
+        {CATEGORIES.map(category => (
+          <button
+            key={category.type}
+            onClick={() => handleCategoryChange(category.type)}
+            className={`text-sm px-3 py-1 bg-slate-100 rounded-3xl ${
+              category.type === categoryName && 'bg-primary text-white'
+            } ${!categoryName && category.type === 'all' && 'bg-primary text-white'}`}
+          >
+            {category.name}
+          </button>
+        ))}
+      </section>
+      <main className="mt-20">{categoryName}에 대한 글</main>
+    </>
+  );
 }

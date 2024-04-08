04d375654dac8fed71dda0eab4226d9230443d78
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-08 20:14:47 +0900

    [#5] feat: NavBar UI 작업 및 클릭시 페이지 이동

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdHome } from 'react-icons/md';
+import { IoMenu } from 'react-icons/io5';
+import { BsChatTextFill } from 'react-icons/bs';
+import { FaBell } from 'react-icons/fa';
+import { IoPersonSharp } from 'react-icons/io5';
 
 export default function NavBar() {
   return (
     <nav className="w-full h-[55px] fixed max-w-[940px] bottom-0 bg-light-gray">
       <ul className="flex justify-between items-center w-full h-full px-5">
-        <li>홈</li>
-        <li>카테고리</li>
-        <li>채팅</li>
-        <li>알림</li>
-        <li>마이페이지</li>
+        <li>
+          <Link to="/">
+            <MdHome className="w-6 h-6" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/category">
+            <IoMenu className="w-6 h-6" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/chat">
+            <BsChatTextFill className="w-6 h-5" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/notification">
+            <FaBell className="w-6 h-5" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/user">
+            <IoPersonSharp className="w-6 h-5" />
+          </Link>
+        </li>
       </ul>
     </nav>
   );

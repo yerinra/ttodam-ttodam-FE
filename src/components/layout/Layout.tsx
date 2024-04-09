6e8de0325f60f532dd4a7ba4e2c9e9db25198e8f
yerinra <yerinaleph@gmail.com>
2024-04-09 15:09:59 +0900

    [#3] design: Outlet에 margin top 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout() {
   return (
     <>
       <Header />
-      <main className="w-full min-h-screen px-5">
+      <main className="w-full min-h-screen px-5 mt-[90px]">
         <Outlet />
       </main>
       <NavBar />

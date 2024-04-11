f15e82664c4ad0d41376daeeb1ead5b41f9bf9ca
yerinra <yerinaleph@gmail.com>
2024-04-11 16:36:41 +0900

    [#11] feat: 게시판 페이지네이션 구현

--- src/pages/PostListPage.tsx
+++ src/pages/PostListPage.tsx
@@ -1,40 +1,68 @@
-import { CATEGORIES } from '../lib/data';
-import { Link, useParams } from 'react-router-dom';
-import type { post, category } from '../lib/types';
-
-import { cn } from '../lib/utils';
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from '@/components/ui/pagination';
 import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
+
+import type { post } from '../lib/types';
+import { CATEGORIES } from '../lib/data';
+import { cn } from '../lib/utils';
+
+import { Pagination, PaginationContent, PaginationItem, PaginationLink } from '@/components/ui/pagination';
+import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
 
 export default function PostListPage() {
   const { selectedCategory } = useParams();
 
   const [data, setData] = useState<post[] | []>([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const apiUrl = selectedCategory === 'all' ? '/post' : `/post/${selectedCategory}`;
-
         const response = await axios.get(apiUrl);
-
         setData(response.data);
-        console.log(response.data);
       } catch (error) {
         console.error('Error fetching data: ', error);
       }
     };
 
+    setCurrentPage(1);
+    setStartPage(1);
     fetchData();
   }, [selectedCategory]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [startPage, setStartPage] = useState(1);
+  const itemsPerPage = 2;
+  const pagesToShow = 5;
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const handleNextPageGroup = () => {
+    if (startPage + pagesToShow <= totalPages) {
+      setStartPage(startPage + pagesToShow);
+    }
+  };
+  const handlePrevPageGroup = () => {
+    if (startPage - pagesToShow >= 1) {
+      setStartPage(startPage - pagesToShow);
+    }
+  };
+  const handlePageClick = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    const { innerText } = e.target as HTMLLIElement;
+    setCurrentPage(+innerText);
+  };
+
+  // const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  const endPage = startPage + pagesToShow - 1;
+  const pages = Array.from({ length: Math.min(pagesToShow, totalPages - startPage + 1) }, (_, i) => startPage + i);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const renderPageNumbers = pages.map(page => (
+    <PaginationItem key={page} onClick={handlePageClick} className={cn({ 'text-primary/90': currentPage == page })}>
+      <PaginationLink className="cursor-pointer">{page}</PaginationLink>
+    </PaginationItem>
+  ));
 
   return (
     <>
@@ -54,8 +82,8 @@ export default function PostListPage() {
 
       <main className="mt-5">
         <section>
-          {data &&
-            data.map((post: post) => (
+          {currentItems &&
+            currentItems.map((post: post) => (
               <Link
                 to={`/post/${post.Id}`}
                 key={post.Id}
@@ -95,27 +123,17 @@ export default function PostListPage() {
         <section className="mt-2 mb-[63px]">
           <Pagination>
             <PaginationContent>
-              <PaginationItem>
-                <PaginationPrevious href="#" />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#">1</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#">2</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#">3</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#">4</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#">5</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationNext href="#" />
-              </PaginationItem>
+              <button onClick={handlePrevPageGroup} disabled={startPage === 1} className="disabled:text-light-gray">
+                <ChevronLeftIcon />
+              </button>
+              {renderPageNumbers}
+              <button
+                onClick={handleNextPageGroup}
+                disabled={endPage >= totalPages}
+                className="disabled:text-light-gray"
+              >
+                <ChevronRightIcon />
+              </button>
             </PaginationContent>
           </Pagination>
         </section>

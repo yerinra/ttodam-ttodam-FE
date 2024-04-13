c0a1ac82b53aedd6b0e29fc89ef85cd965561059
yerinra <yerinaleph@gmail.com>
2024-04-13 15:39:29 +0900

    [#31] feat: 검색 핸들러 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,7 +6,7 @@ export const handlers = [
   http.get('/post', () => {
     return HttpResponse.json(allPosts);
   }),
-  http.get('/post/:param', ({ params }) => {
+  http.get('/post/:param', ({ params, request }) => {
     const { param } = params;
 
     // `param`이 숫자인지 확인하여 `postId`와 `categoryName` 구분.
@@ -16,6 +16,18 @@ export const handlers = [
       // `postId`가 숫자일 경우, 게시글 1개 반환
       const post = allPosts.find((post: Post) => post.Id === postId);
       return HttpResponse.json(post);
+    } else if (param === 'search') {
+      const url = new URL(request.url);
+      const keyword = url.searchParams.get('keyword');
+
+      if (!keyword) return new HttpResponse(null, { status: 404 });
+      const searchResults = allPosts.filter((post: Post) => {
+        return (
+          post.title.toLowerCase().includes(keyword.toLowerCase()) ||
+          post.products.some(product => product.productName.toLowerCase().includes(keyword.toLowerCase()))
+        );
+      });
+      return HttpResponse.json(searchResults);
     } else {
       // `postId`가 숫자가 아닐 경우, 카테고리에 맞는 포스트 목록 반환
       const filteredPosts = allPosts.filter(

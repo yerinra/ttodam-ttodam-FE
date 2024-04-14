121775e311b8cddd4a998eb87f843e82b56b443d
yerinra <yerinaleph@gmail.com>
2024-04-14 20:25:16 +0900

    [#32] feat: StatusBadge 컴포넌트로 분리

--- src/pages/PostDetailPage.tsx
+++ src/pages/PostDetailPage.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import type { Post } from '@/lib/types';
 import axios from 'axios';
+import { cn, formatDate } from '@/lib/utils';
+import { ClockIcon } from '@radix-ui/react-icons';
+import StatusBadge from '@/components/postListPage/StatusBadge';
 
 export default function PostDetailPage() {
   const { postId } = useParams();
@@ -23,5 +26,25 @@ export default function PostDetailPage() {
     fetchData();
   }, [postId]);
 
-  return <section>{data && data?.title}</section>;
+  return (
+    <section>
+      {data && (
+        <div>
+          <div className="">
+            <div className="flex flex-col gap-2 my-4 py-6 border-slate-200 border-y">
+              <StatusBadge status={data.status} variant="detail" />
+              <h1 className="font-bold text-3xl">{data.title}</h1>
+              <div className="flex gap-2 items-center text-slate-500">
+                <ClockIcon />
+                {formatDate(data.createAt)}
+              </div>
+            </div>
+            <div>{data.user.nickname}</div>
+            <div>{data.user.manners * 20}점</div>
+          </div>
+          <div>{data.content}</div>
+        </div>
+      )}
+    </section>
+  );
 }

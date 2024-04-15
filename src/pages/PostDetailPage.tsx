715accc9d37515f1946fcebc920e205b3c36192f
yerinra <yerinaleph@gmail.com>
2024-04-15 18:37:44 +0900

    [#32] design: gap 수정

--- src/pages/PostDetailPage.tsx
+++ src/pages/PostDetailPage.tsx
@@ -3,7 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import type { Category, Post } from '@/lib/types';
 import axios from 'axios';
 import { categoryNameKR, cn, formatDate } from '@/lib/utils';
-import { ClockIcon } from '@radix-ui/react-icons';
+import { BookmarkFilledIcon, ClockIcon } from '@radix-ui/react-icons';
 import StatusBadge from '@/components/postListPage/StatusBadge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
@@ -14,7 +14,7 @@ import Map from '@/components/postDetailPage/Map';
 export default function PostDetailPage() {
   const { postId } = useParams();
   const [data, setData] = useState<Post>();
-
+  const [bookmarked, setBookMarked] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -37,20 +37,30 @@ export default function PostDetailPage() {
     { desc: '만남장소', content: data?.place },
     // { desc: '함께 살 상품', content: data?.products },
   ];
+
+  const handleParticipate = () => {};
+  const handleAddBookmark = () => {};
   return (
     <div className="flex flex-col">
       {data && (
         <>
           {/* 제목 */}
           <section className="flex flex-col gap-2 border-b border-slate-200 py-6">
+            <button className="absolute top-[78px] right-4" onClick={handleAddBookmark}>
+              <BookmarkFilledIcon
+                className={cn(
+                  'h-[80px] w-auto ',
+                  { 'text-slate-200': !bookmarked },
+                  { 'text-yellow-400': !!bookmarked },
+                )}
+              />
+            </button>
             <StatusBadge status={data.status} variant="detail" />
             <h1 className="font-bold text-3xl">{data.title}</h1>
 
             <section className="flex items-center mt-3 mb-3 justify-between">
               <div className="flex items-center gap-2">
-                {data.user.profileImgUrl == '' && (
-                  <div className="w-10 h-10 bg-slate-300 rounded-full">{data.user.profileImgUrl}</div>
-                )}
+                {data.user.profileImgUrl === '' && <div className="w-10 h-10 bg-slate-300 rounded-full" />}
                 {data.user.profileImgUrl !== '' && (
                   <div
                     style={{ backgroundImage: `url(${data.user.profileImgUrl})` }}
@@ -72,6 +82,15 @@ export default function PostDetailPage() {
             <div className="flex gap-2 items-center text-slate-600">
               <ClockIcon />
               {formatDate(data.createAt)}
+              <section className="flex justify-center ml-auto">
+                <Button onClick={handleParticipate} size={'lg'} disabled={data.status !== 'in_progress'}>
+                  {data.status == 'in_progress'
+                    ? '참여신청하기'
+                    : data.status == 'completed'
+                      ? '모집이 완료되었습니다.'
+                      : '마감되었습니다.'}
+                </Button>
+              </section>
             </div>
           </section>
           <section className="flex justify-center">
@@ -101,20 +120,20 @@ export default function PostDetailPage() {
           {/* 메타정보 */}
           <ul className="flex justify-center border-b border-slate-200 py-6 gap-20">
             <div className="ml-6 my-6">
-              <section className="grid grid-cols-1 gap-3 md:grid-cols-2">
-                <section className="flex gap-4">
+              <section className="grid grid-cols-1 gap-5 md:grid-cols-2">
+                <section className="flex gap-3">
                   <div className="text-dark-gray font-bold w-24 h-10 flex-shrink-0">함께 살 상품</div>
-                  <div className="flex flex-col gap-1">
+                  <div className="flex flex-col gap-2">
                     {data &&
                       data.products.map(product => (
                         <div
                           key={product.productId}
-                          className="bg-slate-200 opacity-50 px-2 py-1 rounded-md  hover:bg-gray-300 transition-all"
+                          className="bg-slate-200 opacity-50 px-3 py-1 rounded-md  hover:bg-gray-300 transition-all"
                         >
                           <Link to={product.purchaseLink} className="flex flex-col">
                             <div>{product.productName}</div>
                             <div>
-                              {(product.price / data.participants).toLocaleString()}원 *{' '}
+                              {Math.ceil(product.price / data.participants).toLocaleString()}원 *{' '}
                               {product.count / data.participants}
                               EA
                             </div>
@@ -125,7 +144,7 @@ export default function PostDetailPage() {
                 </section>
                 {data &&
                   metaData.map(meta => (
-                    <li key={meta.desc} className="flex gap-4">
+                    <li key={meta.desc} className="flex gap-5">
                       <div className="text-dark-gray font-bold w-24 flex-shrink-0">{meta.desc}</div>
                       <div
                         className={cn('h-fit', {
@@ -141,7 +160,7 @@ export default function PostDetailPage() {
                     </li>
                   ))}
               </section>
-              {/* <section className="bg-black w-full mt-10 h-48"></section> */}
+
               <KakaoMapScriptLoader>
                 <Map lat={data.pLocationX} lng={data.pLocationY}></Map>
               </KakaoMapScriptLoader>
@@ -149,35 +168,11 @@ export default function PostDetailPage() {
           </ul>
           {/* 상품정보 및 글 */}
           <section className="flex flex-col items-center border-b border-slate-200 py-12 text-slate-800 text-md">
-            {/* {data.productImgUrl.length > 0 && (
-              <Carousel className="w-full max-w-md mb-10">
-                <CarouselContent>
-                  {data.productImgUrl.map((productImg, i) => (
-                    <CarouselItem key={innerHeight}>
-                      <div className="p-1">
-                        <Card>
-                          <CardContent className="flex aspect-square items-center justify-center p-6">
-                            <div
-                              key={i}
-                              style={{ backgroundImage: `url(${productImg})` }}
-                              className="w-24 h-48 justify-center bg-no-repeat bg-cover bg-center"
-                            />
-                          </CardContent>
-                        </Card>
-                      </div>
-                    </CarouselItem>
-                  ))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-              </Carousel>
-            )} */}
-
             <div className="">{data?.content}</div>
           </section>
           {/* 목록으로 가기 버튼 */}
-          <Button size={'lg'}>참여신청하기</Button>
-          <Button className="mt-4 mb-20" variant={'outline'}>
+
+          <Button className="ml-auto mt-4 mb-20" variant={'outline'}>
             <Link to={`/posts/${data?.category.toLowerCase()}`}>목록으로</Link>
           </Button>
         </>

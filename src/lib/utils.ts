8834c96f013e1ca80cebb0ba3a6608a20fd0aa26
yerinra <yerinaleph@gmail.com>
2024-04-15 14:51:24 +0900

    [#32] feat: 카테고리 이름 변환 유틸함수

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
 import { format } from 'date-fns';
 import { twMerge } from 'tailwind-merge';
+import { Category } from './types';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -8,6 +9,21 @@ export function cn(...inputs: ClassValue[]) {
 
 export function formatDate(dateString: string) {
   const date = new Date(dateString);
-  const formattedDate = format(date, 'yyyy/MM/dd');
+  const formattedDate = format(date, 'yyyy-MM-dd');
   return formattedDate;
+}
+
+export function categoryNameKR(category: Exclude<Category, 'ALL'>) {
+  const categoryMap = {
+    DAILY: '생활용품',
+    KITCHEN: '주방용품',
+    FOOD: '식품',
+    CLOTHING: '의류/잡화',
+    HEALTH: '헬스/건강',
+    OFFICE: '문구/오피스',
+    PET: '반려동물',
+    OTHER: '기타',
+  };
+
+  return categoryMap[category] || category;
 }

e44e9c498e88a8cbfbdcb2c0ef1a835ba54c6a4c
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-16 18:57:46 +0900

    [#17] feat: 날짜 형식 'yyyy-mm-dd'로 변경

--- src/components/atoms/DatePicker.tsx
+++ src/components/atoms/DatePicker.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { format } from 'date-fns';
+import { ko } from 'date-fns/locale'
 import { Calendar as CalendarIcon } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
@@ -7,9 +8,20 @@ import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
-export function DatePicker() {
+interface DatePickerProps {
+  onDateChange: (date: Date) => void;
+}
+
+export function DatePicker({ onDateChange }: DatePickerProps) {
   const [date, setDate] = React.useState<Date>();
 
+  const handleDateSelect = (date: Date | undefined) => {
+    if (date) {
+      setDate(date);
+      onDateChange(date);
+    }
+  }
+  
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -20,12 +32,12 @@ export function DatePicker() {
             !date && 'text-muted-foreground',
           )}
         >
-          {date ? format(date, 'PPP') : <span className=" text-base text-[#9ca3af]">마감일</span>}
+          {date ? format(date, 'yyyy-MM-dd') : <span className=" text-base text-[#9ca3af]">마감일</span>}
           <CalendarIcon className="mr-2 h-4 w-4 ml-1 text-[#9ca3af]" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
-        <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+        <Calendar mode="single" locale={ko} selected={date} onSelect={handleDateSelect} initialFocus />
       </PopoverContent>
     </Popover>
   );

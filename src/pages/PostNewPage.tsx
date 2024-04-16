a6f21546918601b6633d5b81c4a27d24b544c994
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-16 17:19:48 +0900

    [#17] feat: 등록 버튼, 카테고리 컴포넌트 -> Form 컴포넌트로 이동

--- src/pages/PostNewPage.tsx
+++ src/pages/PostNewPage.tsx
@@ -1,9 +1,9 @@
 import { FaLongArrowAltLeft } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import Category from '@/components/postNewPage/Category';
 import Form from '@/components/postNewPage/Form';
 
 export default function PostNewPage() {
+
   return (
     <section className="px-5 w-full">
       <div className="flex items-center justify-between border-b border-black h-[60px]">
@@ -13,12 +13,9 @@ export default function PostNewPage() {
           </Link>
           <h3 className="font-semibold">모집글 쓰기</h3>
         </div>
-        <button type="submit" className="py-0.5 px-3 bg-primary rounded-md text-white">
-          등록
-        </button>
+        
       </div>
       <div>
-        <Category />
         <Form />
       </div>
     </section>

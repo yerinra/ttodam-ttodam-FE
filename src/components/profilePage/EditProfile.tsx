6323e68c027c640a5357adad66d19eaee5c0a72d
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-18 01:03:38 +0900

    [#42] design: cursor-pointer 설정

--- src/components/profilePage/EditProfile.tsx
+++ src/components/profilePage/EditProfile.tsx
@@ -6,7 +6,7 @@ export default function EditProfile() {
           <img src="" alt="" className="w-[100px] h-[100px] bg-gray-500 rounded-[50%]" />
           <label
             htmlFor={`edit-image`}
-            className="w-[80px] h-8 flex items-center justify-center py-0.5 px-3 bg-primary rounded-md text-white"
+            className="w-[80px] h-8 flex items-center justify-center py-0.5 px-3 bg-primary rounded-md text-white cursor-pointer"
           >
             사진변경
           </label>

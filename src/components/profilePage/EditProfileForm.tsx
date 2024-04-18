ebf6b60bcf9926999eb68a25843a1598306c62e5
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-18 22:50:56 +0900

    [#42] design: 프로필 수정 페이지 폰트 크기 변경

--- src/components/profilePage/EditProfileForm.tsx
+++ src/components/profilePage/EditProfileForm.tsx
@@ -19,23 +19,23 @@ export default function EditProfileForm() {
         </div>
       </div>
       <div className="w-full flex items-center justify-center gap-5 border-b">
-        <p className="w-[100px]">닉네임</p>
+        <p className="w-[100px] font-bold">닉네임</p>
         <input type="text" placeholder="닉네임" className="outline-none py-4 " />
       </div>
       <div className="w-full flex items-center justify-center py-4 gap-5 border-b text-black">
-        <p className="w-[100px]">비밀번호</p>
+        <p className="w-[100px] font-bold">비밀번호</p>
         <input type="text" placeholder="비밀번호 입력" className="outline-none" />
       </div>
       <div className="w-full flex items-center justify-center py-4 gap-5 border-b text-black">
-        <p className="w-[100px]">비밀번호 확인</p>
+        <p className="w-[100px] font-bold">비밀번호 확인</p>
         <input type="text" placeholder="비밀번호 확인" className="outline-none" />
       </div>
       <div className="w-full flex items-center justify-center py-4 gap-5 border-b text-black">
-        <p className="w-[100px]">주소</p>
+        <p className="w-[100px] font-bold">주소</p>
         <input type="text" placeholder="주소" className="outline-none" />
       </div>
       <div className="w-full flex items-center justify-center py-4 gap-5 border-b text-black">
-        <p className="w-[100px]">전화번호</p>
+        <p className="w-[100px] font-bold">전화번호</p>
         <input type="text" placeholder="전화번호 입력" className="outline-none" />
       </div>
       <button className="w-[80px] h-8 flex items-center justify-center py-0.5 px-3 rounded-md text-white mt-28 bg-[#F94A3F]">

e8380931c1c54db5c76e819025b5f207d84becee
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-19 23:13:45 +0900

    [#42] feat: 핸들러 profile 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,4 +1,5 @@
 import { getBookmarks, postBookmark } from './myPage/bookmark';
+import { getProfiles, postProfile } from './myPage/profile';
 import { getAllPosts, getPostByParam } from './post/postHandlers';
 
-export const handlers = [getAllPosts, getPostByParam, getBookmarks, postBookmark];
+export const handlers = [getAllPosts, getPostByParam, getBookmarks, postBookmark, getProfiles, postProfile];

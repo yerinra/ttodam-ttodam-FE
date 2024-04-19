3e6383ba0afb09d4f91cbba0530c6de851d79f85
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-19 23:59:12 +0900

    [#42] feat: 프로필 mock 데이터 파일 분리

---
+++ src/mocks/mockData/mypage/profile.ts
@@ -0,0 +1,12 @@
+import { Profile } from '@/mocks/handlers/myPage/profile';
+
+export const profile: { profile: Profile[] } = {
+  profile: [
+    {
+      id: 1,
+      nickname: '또담',
+      profileImgUrl: '',
+      manners: 80,
+    },
+  ],
+};

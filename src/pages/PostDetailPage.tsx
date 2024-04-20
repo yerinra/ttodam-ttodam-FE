4023103fbc32c6e47e83137ca474578fcaf33296
yerinra <yerinaleph@gmail.com>
2024-04-20 12:16:02 +0900

    [#47] feat: 포스트 삭제 msw 연결

--- src/pages/PostDetailPage.tsx
+++ src/pages/PostDetailPage.tsx
@@ -1,6 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
-import { getPost } from '@/apis/post/post';
+import { deletePost, getPost } from '@/apis/post/post';
 
 import type { Post } from '@/lib/types';
 
@@ -21,7 +21,25 @@ export default function PostDetailPage() {
     },
   });
   const userInfo = useUserInfoStore(state => state.userInfo);
+  const queryClient = useQueryClient();
   const isUserPost = userInfo && data && userInfo.id === data.user.id;
+
+  const deletePostMutation = useMutation({
+    mutationFn: (postId: number) => deletePost(postId),
+    onSuccess: () => {
+      // 포스트 삭제 성공 시, 쿼리 캐시를 업데이트하고 리스트 페이지로 이동
+      queryClient.invalidateQueries({ queryKey: ['post', postId] });
+      location.href = '/posts/all';
+    },
+    onError: () => {
+      console.error('Error deleting post');
+    },
+  });
+
+  const handleDeletePost = (postId: number) => {
+    // 포스트 삭제 요청
+    data && data.Id && deletePostMutation.mutate(postId);
+  };
 
   if (error) return <div>에러가 발생했습니다.</div>;
   if (isLoading) return <div>Loading...</div>;
@@ -30,7 +48,7 @@ export default function PostDetailPage() {
     <div className="flex flex-col">
       {data && (
         <>
-          <PostHeader data={data} isUserPost={isUserPost} />
+          <PostHeader data={data} isUserPost={isUserPost} handleDeletePost={handleDeletePost} />
           <ProductImg data={data} />
           <PostMetaDataSection data={data} />
           <ContentSection data={data} />

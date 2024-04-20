c9bd13633386acea97fddcd5bfc2843508282795
yerinra <yerinaleph@gmail.com>
2024-04-20 15:44:03 +0900

    [#47] refactor: 기존 helpers.ts에서 utils.ts로 이동

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,3 +27,10 @@ export function categoryNameKR(category: Exclude<Category, 'ALL'>) {
 
   return categoryMap[category] || category;
 }
+
+const priceFormat = new Intl.NumberFormat('ko-KR', {
+  style: 'decimal',
+  maximumFractionDigits: 0,
+});
+
+export const toPriceFormat = (value: number) => priceFormat.format(value);

abe8e73178d9b18f24e9fc31004aa6e7e4244932
yerinra <yerinaleph@gmail.com>
2024-04-23 13:23:35 +0900

    [#3] design: 글쓰기 아이콘 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 // import { IoBookmarkSharp } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 import KakaoMapScriptLoader from '../map/KakaoMapScriptLoader';
@@ -7,6 +7,7 @@ import MapSearchInput from '../map/MapSearchInput';
 
 import MapMarkerController from '../map/MapMarkerController';
 import { PlaceType } from '@/types/map';
+import { Pencil1Icon } from '@radix-ui/react-icons';
 // import { IoBookmarkSharp } from 'react-icons/io5';
 
 // 상품 이름, 주소, 모집인원 데이터에 맞춰서 불러오기
@@ -29,16 +30,12 @@ export default function HomePage() {
           />
           <Link
             to="/post/new"
-            className="absolute right-5 bottom-5 z-[10] flex items-center justify-center w-10 h-10 border border-black rounded-[50%] bg-black text-white"
+            className="absolute right-5 bottom-5 z-[10] flex items-center justify-center w-10 h-10 border rounded-[50%] bg-black text-white"
           >
-            +
+            <Pencil1Icon className="h-4 w-4" />
           </Link>
         </Map>
       </KakaoMapScriptLoader>
-      {/* <Link
-        to="/post/new"
-        className="absolute bottom-[100px] right-14 flex items-center justify-center w-10 h-10 border border-black rounded-[50%] bg-black text-white"
-      > */}
     </div>
   );
 }

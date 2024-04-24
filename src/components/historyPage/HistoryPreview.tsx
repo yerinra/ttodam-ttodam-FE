7de56ca43d6fb4c6eda13dcaf608017473e29aba
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-25 02:24:00 +0900

    [#43] feat: HistoryPreview 파일 추가

---
+++ src/components/historyPage/HistoryPreview.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import StatusBadge from '../postListPage/StatusBadge';
+import { Post } from '@/types/post';
+
+type HistoryPreviewProps = {
+  post: Post;
+};
+
+export default function HistoryPreview({ post }: HistoryPreviewProps) {
+  return (
+    <li
+      key={post.Id}
+      className="relative flex flex-col border-light-gray first-of-type:border-t-[1px] border-b-[1px] p-4 hover:bg-secondary gap-y-2 transition-all"
+    >
+      <div className="flex items-center gap-x-2">
+        <StatusBadge status={post.status} />
+
+        <Link to={`/post/${post.Id}`} className="font-bold">
+          {post.title}
+        </Link>
+      </div>
+      <div className="flex">
+        <ul className="flex gap-2 text-sm items-center opacity-50">
+          {post.products.map(product => (
+            <li className="px-[6px] py-[2px]  bg-slate-200 rounded-sm" key={product.productId}>
+              {`${product.productName} ${(product.price / product.count).toLocaleString()}원`}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </li>
+  );
+}

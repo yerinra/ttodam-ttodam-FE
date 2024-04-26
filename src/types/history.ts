06158853cc35ef0eeef523173b2b5dccd44dfbe2
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-26 22:56:41 +0900

    [#43] feat: 참여내역 type 수정

--- src/types/history.ts
+++ src/types/history.ts
@@ -1,21 +1,16 @@
-import { Category, Product, Status, User } from './post';
+import { Product, Status } from './post';
+
+export type HistoryResponse = {
+  list: History[];
+};
 
 export type History = {
-  Id: number;
-  user: User;
   postId: number;
   title: string;
   status: Status;
-  category: Category;
-  place: string;
-  pLocationX: number;
-  pLocationY: number;
-  content: string;
   products: Product[];
-  productImgUrl: string[] | [];
-  participants: number;
   startDate: string;
   endDate: string;
   members: string[];
-  isHost: number;
+  master: number;
 };

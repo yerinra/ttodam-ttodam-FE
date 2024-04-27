b18a4030703ad928036b38d3e2f8e3d068950bcc
yerinra <yerinaleph@gmail.com>
2024-04-27 17:19:20 +0900

    [#65] feat: 임시 msw 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser.ts');
-  await worker.start({
-    onUnhandledRequest: 'bypass',
-  });
-}
+// if (import.meta.env.DEV) {
+const { worker } = await import('./mocks/browser.ts');
+await worker.start({
+  onUnhandledRequest: 'bypass',
+});
+// }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>

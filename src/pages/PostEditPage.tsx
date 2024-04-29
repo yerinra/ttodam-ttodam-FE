c6b0c833b97ae5b65effbe1f18b7f8f12ea4c58b
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-04-29 14:44:22 +0900

    [#78] feat: 게시글 수정 페이지 생성

--- src/pages/PostEditPage.tsx
+++ src/pages/PostEditPage.tsx
@@ -1,6 +1,38 @@
+import Form from '@/components/postEditPage/Form';
+import { FaLongArrowAltLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
+import { getPost } from '@/apis/post/post';
+import type { Post } from '@/types/post';
 
 export default function PostEditPage() {
   const { postId } = useParams();
-  return <div>게시글 {postId} 수정 페이지</div>;
+
+  const { data, error, isLoading } = useQuery<Post>({
+    queryKey: ['post', postId],
+    queryFn: () => {
+      return getPost(+postId!);
+    },
+  });
+
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    // <div>게시글 {postId} 수정 페이지</div>
+    <section className="px-5 w-full">
+      <div className="flex items-center justify-between border-b border-black h-[60px]">
+        <div className="flex items-center gap-2.5">
+          <Link to="/posts/all">
+            <FaLongArrowAltLeft className="w-5 h-5" />
+          </Link>
+          <h3 className="font-semibold">모집글 수정</h3>
+        </div>
+      </div>
+      <div>
+        <Form data={data!} />
+      </div>
+    </section>
+  );
 }

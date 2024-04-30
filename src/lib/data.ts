dc5f277eafd2131fc283b11d9c59d058a222db6f
yerinra <yerinaleph@gmail.com>
2024-04-30 21:22:19 +0900

    [#11] refactor: 카테고리 게시글 조회시 카테고리 이름 한글로 수정

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -17,9 +17,9 @@ export const CATEGORIES = [
   { type: 'KITCHEN', name: '주방용품', icon: React.createElement(LuUtensils) },
   { type: 'FOOD', name: '식품', icon: React.createElement(LuSalad) },
   { type: 'CLOTHING', name: '의류/잡화', icon: React.createElement(LuWatch) },
-  { type: 'HEALTH', name: '헬스/건강', icon: React.createElement(LuPill) },
+  { type: 'HEALTH', name: '헬스/건강식품', icon: React.createElement(LuPill) },
   { type: 'OFFICE', name: '문구/오피스', icon: React.createElement(LuPencilRuler) },
-  { type: 'PET', name: '반려동물', icon: React.createElement(LuDog) },
+  { type: 'PET', name: '반려동물용품', icon: React.createElement(LuDog) },
   { type: 'OTHER', name: '기타', icon: React.createElement(LuClover) },
 ] as const;
 

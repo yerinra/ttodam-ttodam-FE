f29355a1c872480ba6d52c987918429f7699b560
yerinra <yerinaleph@gmail.com>
2024-04-30 21:23:52 +0900

    [#65] fix: API 변경에 따른 코드 수정

--- src/mocks/handlers/auth/signup.ts
+++ src/mocks/handlers/auth/signup.ts
@@ -1,18 +1,18 @@
 import { HttpResponse, http } from 'msw';
 
-export const verifyEmailHandler = http.post('/users/signup', async ({ request }) => {
+export const signUpHandler = http.post('users/signup', async () => {
+  return new HttpResponse(null, { status: 200 });
+});
+export const verifyEmailHandler = http.post('/users/signup/sendmail', async ({ request }) => {
   const url = new URL(request.url);
-  const sendEmail = url.searchParams.get('sendEmail');
-  console.log(url);
-  if (url.searchParams.size == 0) {
-    // 회원가입
-    return new HttpResponse(null, { status: 200 });
-  } else if (!sendEmail) {
+  const email = url.searchParams.get('email');
+
+  if (!email) {
     // 이메일 인증시 이메일 비워둔 경우
     return new HttpResponse(null, { status: 400 });
   } else {
     // 메일 인증
-    if (sendEmail === 'test@test.com') {
+    if (email === 'test@test.com') {
       // 회원 메일 인증 성공 응답 반환
       return new HttpResponse(null, { status: 200 });
     } else {

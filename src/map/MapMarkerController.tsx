ad6cba36439fe2d8b793592b5b202e7db2476721
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-05-02 02:53:19 +0900

    [#5] feat: 마커 데이터 수정

--- src/map/MapMarkerController.tsx
+++ src/map/MapMarkerController.tsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react';
 import MapMarker from './MapMarker';
 import { useMap } from './useMap';
 import { PlaceType } from '@/types/map';
-import { PostMap } from '@/types/post';
 import { axiosAccessFn } from '@/apis/apiClient';
+import { PostMap } from '@/types/post';
 
 interface MapMarkerControllerProps {
   places: PlaceType[];
@@ -15,7 +15,7 @@ export default function MapMarkerController(props: MapMarkerControllerProps) {
   const map = useMap();
 
   const axiosAccess = axiosAccessFn();
-  const [, setMapPosts] = useState<PostMap[]>([]);
+  const [mapPosts, setMapPosts] = useState<PostMap[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -51,46 +51,7 @@ export default function MapMarkerController(props: MapMarkerControllerProps) {
   return (
     <>
       {props.places.map(place => {
-        return (
-          <MapMarker
-            key={place.id}
-            place={place}
-            showInfo={props.selectedId === place.id}
-            data={{
-              postList: {
-                postId: 59,
-                status: 'IN_PROGRESS',
-                category: 'DAILY',
-                authorId: 3,
-                authorNickname: '유저3',
-                title: '물티슈와 휴지 같이 구매하실 분',
-                content: '물티슈와 휴지를 같이 삽시다!!',
-                products: [
-                  {
-                    productName: '물티슈',
-                    price: 12000,
-                    count: 10,
-                  },
-                  {
-                    productName: '휴지',
-                    price: 3000,
-                    count: 2,
-                  },
-                ],
-                createdAt: '2024-04-18T07:05:40',
-                updatedAt: '2024-04-26T16:22:11',
-                place: '서울특별시 마포구 양화로 지하188',
-                plocationX: 37.555573,
-                plocationY: 126.924748,
-                purchaseStatus: 'FAILURE',
-                bookmarkId: false,
-              },
-              loginUserLocation: '서울특별시 마포구 양화로 지하188',
-              loginUserLocationX: 37.555573,
-              loginUserLocationY: 126.924748,
-            }}
-          />
-        );
+        return <MapMarker key={place.id} place={place} showInfo={props.selectedId === place.id} mapPosts={mapPosts} />;
       })}
     </>
   );

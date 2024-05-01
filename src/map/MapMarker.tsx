4e5805a67c601ac2937c0fbf29627ee7450da491
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-05-02 02:50:17 +0900

    [#5] feat: 데이터 불러오기

--- src/map/MapMarker.tsx
+++ src/map/MapMarker.tsx
@@ -4,17 +4,20 @@ import { useEffect, useLayoutEffect, useMemo, useRef } from 'react';
 import { useMap } from './useMap';
 import { IoBookmarkSharp } from 'react-icons/io5';
 import { PlaceType } from '@/types/map';
+import Badge from '@/components/atoms/Badge';
 import { PostMap } from '@/types/post';
 
 interface MapMarkerProps {
   place: PlaceType;
   showInfo?: boolean;
-  data: PostMap;
+  mapPosts: PostMap[];
 }
 
+// TODO: 게시글이 있는 주소의 마커만 생성하기
 export default function MapMarker(props: MapMarkerProps) {
   const map = useMap();
   const container = useRef(document.createElement('div'));
+  // const [mapPosts] = useState<PostMap[]>([]);
 
   const infoWindow = useMemo(() => {
     return new kakao.maps.CustomOverlay({
@@ -31,7 +34,7 @@ export default function MapMarker(props: MapMarkerProps) {
 
     kakao.maps.event.addListener(marker, 'click', function () {
       map.setCenter(props.place.position);
-      map.setLevel(4, {
+      map.setLevel(2, {
         animate: true,
       });
       infoWindow.setMap(map);
@@ -69,13 +72,15 @@ export default function MapMarker(props: MapMarkerProps) {
           }}
           className="absolute bottom-[60px] left-0 ml-[-120px] flex flex-col gap-2 w-[260px] px-7 py-4 border border-black rounded-2xl bg-white"
         >
-          <strong>{props.data.postList.title}</strong>
-          <p className="text-sm">{props.place.address}</p>
-          <p className="text-sm font-bold">
-            모집 상태
-            <span className="text-sm ml-1">{props.data.postList.status}</span>
-          </p>
-          <IoBookmarkSharp className="absolute top-[-4px] right-[20px] w-8 h-9 text-yellow-300" />
+          {props.mapPosts &&
+            props.mapPosts.map(post => (
+              <div key={post.postList.postId}>
+                <strong>{post.postList.title}</strong>
+                <p className="text-sm">{post.postList.place}</p>
+                <Badge variant={post.postList.status}></Badge>
+                <IoBookmarkSharp className="absolute top-[-4px] right-[20px] w-8 h-9 text-yellow-300" />
+              </div>
+            ))}
         </button>,
         container.current,
       )

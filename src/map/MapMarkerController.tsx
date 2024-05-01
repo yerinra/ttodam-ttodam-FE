f035adb2a4213ec11ad875dd523920e34bb657ce
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-05-02 00:43:10 +0900

    [#5] feat: get 요청

--- src/map/MapMarkerController.tsx
+++ src/map/MapMarkerController.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import MapMarker from './MapMarker';
 import { useMap } from './useMap';
 import { PlaceType } from '@/types/map';
+import { PostMap } from '@/types/post';
+import { axiosAccessFn } from '@/apis/apiClient';
 
 interface MapMarkerControllerProps {
   places: PlaceType[];
@@ -11,6 +13,24 @@ interface MapMarkerControllerProps {
 
 export default function MapMarkerController(props: MapMarkerControllerProps) {
   const map = useMap();
+
+  const axiosAccess = axiosAccessFn();
+  const [, setMapPosts] = useState<PostMap[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosAccess({
+          method: 'get',
+          url: 'post/map/list',
+        });
+        setMapPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (props.places.length < 1) {
@@ -31,7 +51,46 @@ export default function MapMarkerController(props: MapMarkerControllerProps) {
   return (
     <>
       {props.places.map(place => {
-        return <MapMarker key={place.id} place={place} showInfo={props.selectedId === place.id} />;
+        return (
+          <MapMarker
+            key={place.id}
+            place={place}
+            showInfo={props.selectedId === place.id}
+            data={{
+              postList: {
+                postId: 59,
+                status: 'IN_PROGRESS',
+                category: 'DAILY',
+                authorId: 3,
+                authorNickname: '유저3',
+                title: '물티슈와 휴지 같이 구매하실 분',
+                content: '물티슈와 휴지를 같이 삽시다!!',
+                products: [
+                  {
+                    productName: '물티슈',
+                    price: 12000,
+                    count: 10,
+                  },
+                  {
+                    productName: '휴지',
+                    price: 3000,
+                    count: 2,
+                  },
+                ],
+                createdAt: '2024-04-18T07:05:40',
+                updatedAt: '2024-04-26T16:22:11',
+                place: '서울특별시 마포구 양화로 지하188',
+                plocationX: 37.555573,
+                plocationY: 126.924748,
+                purchaseStatus: 'FAILURE',
+                bookmarkId: false,
+              },
+              loginUserLocation: '서울특별시 마포구 양화로 지하188',
+              loginUserLocationX: 37.555573,
+              loginUserLocationY: 126.924748,
+            }}
+          />
+        );
       })}
     </>
   );

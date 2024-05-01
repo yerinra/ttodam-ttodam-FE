22dc1ffd2608798765a554666cbab21b8d1e78ac
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-05-02 02:52:03 +0900

    [#5] fix: 검색 리스트 영역 주석 처리

--- src/map/MapSearchInput.tsx
+++ src/map/MapSearchInput.tsx
@@ -1,23 +1,27 @@
 import React, { FormEvent, useEffect, useRef, useState } from 'react';
 
-import { useMap } from './useMap';
+// import { useMap } from './useMap';
 import { PlaceType } from '@/types/map';
+import { PostMap } from '@/types/post';
 
 interface MapSearchInputProps {
   onUpdatePlaces: (places: PlaceType[]) => void;
   onSelect: (placeId: string) => void;
+  posts: PostMap[];
 }
 
 // 더미 데이터 만들기 전까지는 지역으로 검색 가능. 추후에 데이터 생성 후 게시글 키워드로 수정
-// 지역 검색이여서 기존 SearchInput은 그대로 두고 따로 컴포넌트 분리, 추후에 리팩토링해야 함.
+// 검색 리스트 영역 데이터 불러와서 화면에 보여주기
 export default function MapSearchInput(props: MapSearchInputProps) {
-  const map = useMap();
+  // const map = useMap();
   const [search, setSearch] = useState('');
-  const [places, setPlaces] = useState<PlaceType[]>([]);
-  const [isSearchInput, setIsSearchInput] = useState(false);
+  // const [places, setPlaces] = useState<PlaceType[]>([]);
+  // const [isSearchInput, setIsSearchInput] = useState(false);
+  const [, setPlaces] = useState<PlaceType[]>([]);
+  const [, setIsSearchInput] = useState(false);
 
   const placeService = useRef<kakao.maps.services.Places | null>(null);
-  const listRef = useRef<HTMLUListElement>(null);
+  // const listRef = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
     if (placeService.current) {
@@ -80,17 +84,16 @@ export default function MapSearchInput(props: MapSearchInputProps) {
   };
 
   // 리스트 클릭시 위치 이동
-  const handleItemClick = (place: PlaceType) => {
-    map.setCenter(place.position);
-    map.setLevel(4);
-    props.onSelect(place.id);
+  // const handleItemClick = (place: PlaceType) => {
+  //   map.setCenter(place.position);
+  //   map.setLevel(2);
+  //   props.onSelect(place.id);
 
-    // 리스트 영역 닫힘
-    setIsSearchInput(false);
-  };
+  //   // 리스트 영역 닫힘
+  //   setIsSearchInput(false);
+  // };
 
   return (
-    // <div className="fixed top-[89px] left-0 flex flex-col min-w-[240px] w-full h-[70px] z-[99999] px-5 bg-white my-0 mx-auto">
     <div className="absolute top-0 flex flex-col min-w-[240px] w-full h-[70px] z-[99999] px-5 bg-white my-0 mx-auto">
       <form onSubmit={handleSubmit} className="sticky min-w-[240px] w-full max-w-[768px] h-11 my-0 mx-auto">
         <input
@@ -101,27 +104,27 @@ export default function MapSearchInput(props: MapSearchInputProps) {
           className="min-w-[240px] w-full max-w-[768px] h-11 border border-black rounded hover:border-primary focus:border-primary px-3 py-1 focus:outline-none text-sm search-input"
         />
       </form>
-      {isSearchInput && (
+      {/* {isSearchInput && (
         <ul
           ref={listRef}
           className="absolute top-[50px] left-[50%] translate-x-[-50%] min-w-[240px] w-full max-w-[768px] h-40 bg-white overflow-y-auto map-search-list"
         >
-          {places.map((item, index) => {
+          {props.posts.map((item, index) => {
             return (
-              <li
-                key={item.id}
+              <button
+                key={item.postList.postId}
                 onClick={() => {
                   handleItemClick(item);
                 }}
                 className="min-w-[240px] w-full max-w-[768px] h-[60px] bg-white flex flex-col border-b hover:bg-gray-100 py-0 pr-0 pl-3.5"
               >
-                <label>{`${index + 1}. ${item.title}`}</label>
-                <span>{item.address}</span>
-              </li>
+                <label>{`${index + 1}. ${item.postList.title}`}</label>
+                <span>{item.postList.place}</span>
+              </button>
             );
           })}
         </ul>
-      )}
+      )} */}
     </div>
   );
 }

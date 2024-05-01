dfe0a4d409a9316b6ce452c95ab6bb73aaa48909
yerinra <yerinaleph@gmail.com>
2024-05-01 14:41:17 +0900

    [#6] refactor: 카카오 로그인 버튼 이름 수정

--- src/components/Login/KakaoLogin.tsx
+++ src/components/Login/KakaoLogin.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const KakaoLogin = () => {
   return (
     <Link to={KAKAO_AUTH_URL} className="flex flex-col space-y-4">
-      <button className=" bg-yellow-300 text-black px-10 py-4 rounded w-96">카카오톡 로그인</button>
+      <button className=" bg-yellow-300 text-black px-10 py-4 rounded w-96">카카오 로그인</button>
     </Link>
   );
 };

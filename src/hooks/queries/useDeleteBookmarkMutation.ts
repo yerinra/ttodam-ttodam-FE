db1b864069b729b2816208e2b7385cd55486060f
yerinra <yerinaleph@gmail.com>
2024-05-01 11:26:27 +0900

    [#32] fix: 북마크 삭제시 postId에서 bookmarkId로 수정

--- src/hooks/queries/useDeleteBookmarkMutation.ts
+++ src/hooks/queries/useDeleteBookmarkMutation.ts
@@ -1,21 +1,30 @@
 import { deleteBookmark } from '@/apis/myPage/bookmark';
-import { BookMark } from '@/types/bookmark';
+import useCurrentPostIdStore from '@/store/currentPostIdStore';
+// import { BookMark } from '@/types/bookmark';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-export const useDeleteBookmarkMutation = () => {
+export const useDeleteBookmarkMutation = (isInDetailPage: boolean) => {
   const queryClient = useQueryClient();
+  const { currentPostId } = useCurrentPostIdStore();
+  // let handleBookmarkUpdate;
 
+  // if (isInDetailPage) {
+  //   handleBookmarkUpdate = (deletedBookmarkId: number) => {
+  //     const previousData = queryClient.getQueryData<BookMark[]>(['bookmarks']);
+  //     queryClient.setQueryData(['bookmarks'], previousData => {
+  //       return (previousData as BookMark[])?.filter((item: BookMark) => item.id !== deletedBookmarkId) || [];
+  //     });
+  //     return { previousData };
+  //   };
+  // } else {
+  //   handleBookmarkUpdate = () => {};
+  // }
   return useMutation({
     mutationFn: (bookmarkId: number) => deleteBookmark(bookmarkId),
-    onMutate: (deletedBookmarkId: number) => {
-      const previousData = queryClient.getQueryData<BookMark[]>(['bookmarks']);
-      queryClient.setQueryData(['bookmarks'], previousData => {
-        return (previousData as BookMark[])?.filter((item: BookMark) => item.id !== deletedBookmarkId) || [];
-      });
-      return { previousData };
-    },
+    // onMutate: handleBookmarkUpdate,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['bookmarks'] });
+      if (!isInDetailPage) queryClient.invalidateQueries({ queryKey: ['bookmarks'] });
+      else queryClient.invalidateQueries({ queryKey: ['post', currentPostId] });
     },
     onError: () => {
       console.log('error!');

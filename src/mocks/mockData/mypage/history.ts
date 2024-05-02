dab1ec907d827051c4f7b8a7dffe9cb0c0f2530f
yerinra <yerinaleph@gmail.com>
2024-05-02 17:01:31 +0900

    [#43] refactor: 참여내역 조회 리팩토링

--- src/mocks/mockData/mypage/history.ts
+++ src/mocks/mockData/mypage/history.ts
@@ -1,35 +1,33 @@
-export const HistoryMockData = {
-  list: [
-    {
-      postId: 59,
-      userId: 3,
-      userNickname: '유저3',
-      authorId: 63,
-      authorNickname: '유저11',
-      title: '물티슈와 휴지 같이 구매하실 분',
-      updatedAt: '2024-04-18 07:05:40',
-      createdAt: '2024-04-26 16:22:11',
-      status: 'COMPLETED',
-      requestStatus: 'REFUSE',
-      purchaseStatus: 'SUCCESS',
-      products: [
-        {
-          productId: 38,
-          postId: 59,
-          productName: '물티슈',
-          count: 10,
-          purchaseLink: 'https://smartstore.naver.com/sskorea777/products/6364217638',
-          price: 12000,
-        },
-        {
-          productId: 39,
-          postId: 59,
-          productName: '휴지',
-          count: 2,
-          purchaseLink: 'https://smartstore.naver.com/sskorea777/products/6364217638',
-          price: 3000,
-        },
-      ],
-    },
-  ],
-};
+export const HistoryMockData: History[] = [
+  {
+    postId: 59,
+    userId: 3,
+    userNickname: '유저3',
+    authorId: 63,
+    authorNickname: '유저11',
+    title: '물티슈와 휴지 같이 구매하실 분',
+    updatedAt: '2024-04-18 07:05:40',
+    createdAt: '2024-04-26 16:22:11',
+    status: 'COMPLETED',
+    requestStatus: 'REFUSE',
+    purchaseStatus: 'SUCCESS',
+    products: [
+      {
+        // productId: 38,
+        // postId: 59,
+        productName: '물티슈',
+        count: 10,
+        // purchaseLink: 'https://smartstore.naver.com/sskorea777/products/6364217638',
+        price: 12000,
+      },
+      {
+        // productId: 39,
+        // postId: 59,
+        productName: '휴지',
+        count: 2,
+        // purchaseLink: 'https://smartstore.naver.com/sskorea777/products/6364217638',
+        price: 3000,
+      },
+    ],
+  },
+];

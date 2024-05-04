bf93a5e291675381f89d7f9b23bbd38d4ce4eb82
yerinra <yerinaleph@gmail.com>
2024-05-04 23:29:47 +0900

    [#9] design: 화면 높이 수정

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -6,7 +6,7 @@ import SocialLogin from '../components/LoginPage/SocialLogin';
 
 const SignUpPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen my-20">
+    <div className="flex flex-col items-center justify-center h-screen">
       <img src={logo} alt="Logo" className="w-24 h-24 mb-8" />
       <h1 className="text-4xl font-bold mb-8">회원가입</h1>
       <SignUpForm />

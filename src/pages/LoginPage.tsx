c9b085c491e163d949695a89ecb7d827b166109e
yerinra <yerinaleph@gmail.com>
2024-05-04 23:29:40 +0900

    [#6] design: 화면 높이 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
   }, [isLoggedIn]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen my-5">
+    <div className="flex flex-col items-center justify-center h-screen">
       <Logo />
       <h1 className="text-4xl font-bold mb-8">로그인</h1>
       <LoginForm />

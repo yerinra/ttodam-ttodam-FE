0d300089ba075fedef48c826715fc0e5715e3818
yerinra <yerinaleph@gmail.com>
2024-05-07 21:06:55 +0900

    [#23] refactor: chatting에서 chat으로 폴더 이름 변경

--- src/apis/chat/socket.ts
+++ src/apis/chat/socket.ts
@@ -5,15 +5,14 @@ const SOCKET_URL: string = '서버 url';
 let socket: Socket;
 
 export const connectToChatRoom = (chatroomId: string, callback: (users: any[]) => void): void => {
-  socket = io(`${SOCKET_URL}/ws-chatting`);
-  socket.on('connect', () => {
-    socket.emit('subscribe', `/chatroom/${chatroomId}`);
-  });
-
-  socket.on('message', (response: any) => {
-    const users = JSON.parse(response);
-    callback(users);
-  });
+  // socket = io(`${SOCKET_URL}/ws-chatting`);
+  // socket.on('connect', () => {
+  //   socket.emit('subscribe', `/chatroom/${chatroomId}`);
+  // });
+  // socket.on('message', (response: any) => {
+  //   const users = JSON.parse(response);
+  //   callback(users);
+  // });
 };
 
 export const disconnectFromChatRoom = (): void => {

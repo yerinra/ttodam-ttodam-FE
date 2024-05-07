f3c3a94ecc103cd23df2d85d5260054d8e112ae4
yerinra <yerinaleph@gmail.com>
2024-05-07 21:05:50 +0900

    [#12] feat: formatAgo 유틸 함수 구현

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,8 @@
 import { Category } from '@/types/post';
 import { type ClassValue, clsx } from 'clsx';
-import { format } from 'date-fns';
+import { format, formatDistanceToNow } from 'date-fns';
+import { ko } from 'date-fns/locale/ko';
 import { twMerge } from 'tailwind-merge';
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -47,4 +47,19 @@ export const isMatchingPath = (path: string, pathname: string) => {
   } else {
     return pathname.includes(path);
   }
+};
+
+export const formatAgo = (date: string) => {
+  const d = new Date(date);
+  const now = Date.now();
+  const diff = (now - d.getTime()) / 1000; // 현재 시간과의 차이(초)
+  if (diff < 60 * 1) {
+    // 1분 미만일땐 방금 전 표기
+    return '방금 전';
+  }
+  if (diff < 60 * 60 * 24 * 3) {
+    // 3일 미만일땐 시간차이 출력(몇시간 전, 몇일 전)
+    return formatDistanceToNow(d, { addSuffix: true, locale: ko });
+  }
+  return format(d, 'PPP EEE p', { locale: ko }); // 날짜 포맷
 };

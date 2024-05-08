c18df7842770719f3334ffcceb3bbdcda142c0ae
yerinra <yerinaleph@gmail.com>
2024-05-09 04:22:42 +0900

    [#17] feat: PostForm 컴포넌트 리액트 훅 폼과 zod로 구현

--- src/components/atoms/DaumPost.tsx
+++ src/components/atoms/DaumPost.tsx
@@ -1,8 +1,14 @@
-import { PostNew } from '@/types/post';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, RefObject, forwardRef, useState } from 'react';
 import DaumPostcode from 'react-daum-postcode';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
 
-export default function DaumPost({ onAddressChange }: { onAddressChange: (address: PostNew['place']) => void }) {
+type DaumPostProps = {
+  onChange: (value: string) => void;
+  forwardedRef?: RefObject<HTMLInputElement> | ((instance: HTMLInputElement | null) => void) | null;
+};
+
+const DaumPost = forwardRef<HTMLInputElement, DaumPostProps>(({ onChange }, forwardedRef) => {
   // 주소, 모달 열림 여부, 상세 주소 상태 관리
   const [address, setAddress] = useState('');
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -12,10 +18,7 @@ export default function DaumPost({ onAddressChange }: { onAddressChange: (addres
     const { address } = data;
     setIsOpen(false);
     setAddress(address);
-    onAddressChange(address);
-
-    // 데이터 확인
-    console.log(data, 'data');
+    onChange(address);
   };
 
   // 주소 검색 모달이 닫힐 때 호출되는 콜백 함수
@@ -23,10 +26,8 @@ export default function DaumPost({ onAddressChange }: { onAddressChange: (addres
     // 모달이 강제로 닫힌 경우 또는 주소 선택 완료 후 모달 닫기
     if (state === 'FORCE_CLOSE') {
       setIsOpen(false);
-      console.log('FORCE_CLOSE');
     } else if (state === 'COMPLETE_CLOSE') {
       setIsOpen(false);
-      console.log('COMPLETE_CLOSE');
     }
   };
 
@@ -41,29 +42,26 @@ export default function DaumPost({ onAddressChange }: { onAddressChange: (addres
   };
 
   return (
-    <div>
-      <div className="flex items-center justify-between w-full relative border-b">
-        <input
-          type="text"
-          placeholder="희망 거래 주소를 선택해주세요."
-          value={address}
-          onChange={handleInputChange}
-          readOnly
-          className="w-2/4 outline-none py-4"
-        />
-        <button
-          type="button"
-          onClick={toggleHandler}
-          className="w-[90px] py-1 px-3 bg-primary border-none rounded-md text-white outline-0"
-        >
-          주소 찾기
-        </button>
-        {isOpen && (
-          <div className="absolute left-0 top-full border">
-            <DaumPostcode onComplete={handleComplete} onClose={handleClose} autoMapping />
-          </div>
-        )}
-      </div>
+    <div className="flex items-center justify-between w-full relative gap-2">
+      <Input
+        type="text"
+        ref={forwardedRef}
+        placeholder="희망 거래 주소를 선택해주세요."
+        value={address}
+        onChange={handleInputChange}
+        readOnly
+      />
+      <Button type="button" onClick={toggleHandler} variant="secondary">
+        주소 찾기
+      </Button>
+      {isOpen && (
+        <div className="absolute left-0 top-full w-full border z-[100]">
+          <DaumPostcode onComplete={handleComplete} onClose={handleClose} autoMapping />
+        </div>
+      )}
     </div>
   );
-}
+});
+
+DaumPost.displayName = 'DaumPost';
+export default DaumPost;

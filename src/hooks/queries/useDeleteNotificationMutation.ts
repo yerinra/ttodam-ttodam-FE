25ba4edea956c6113124cc04a5b2394256dd5834
yerinra <yerinaleph@gmail.com>
2024-05-10 22:03:07 +0900

    [#53] feat: 알림 타입 정의

--- src/hooks/queries/useDeleteNotificationMutation.ts
+++ src/hooks/queries/useDeleteNotificationMutation.ts
@@ -1,5 +1,6 @@
 import { deleteNotification } from '@/apis/notification/notification';
-import { Notification } from '@/mocks/mockData/notification/notifications';
+import { Notification } from '@/types/notification';
+
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useDeleteNotificationMutation = () => {

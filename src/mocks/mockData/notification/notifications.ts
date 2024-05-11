2a62862e4011196c2e78ec228d3788df9838b184
yerinra <yerinaleph@gmail.com>
2024-05-11 19:59:44 +0900

    [#53] feat: mock data 알림 id 수정

--- src/mocks/mockData/notification/notifications.ts
+++ src/mocks/mockData/notification/notifications.ts
@@ -5,14 +5,14 @@ export const notificationsMockData: Notification[] = [
     notificationId: 7,
     userId: 63,
     type: 'KEYWORD',
-    postId: 38,
+    postId: 59,
     createAt: '2024-04-27T16:01:09',
   },
   {
     notificationId: 8,
     userId: 63,
     type: 'KEYWORD',
-    postId: 38,
+    postId: 60,
     createAt: '2024-04-27T16:01:09',
   },
 ];
